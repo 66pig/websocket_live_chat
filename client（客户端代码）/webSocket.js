9aea99c53ac1ab271be03307a04ad57e5dc997cf
21065238@qq.com <GitHub>
2020-01-15 18:12:58 +0800

    加入压缩版本的客户端封装

--- client（客户端代码）/webSocket.js
+++ client（客户端代码）/webSocket.js
@@ -1,1 +1,50 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('2 w(1){2 d(1={}){8(!1.3){6.n(\'x\')}8(!1.a){6.n(\'v\')}0.a=1.a;0.3=1.3.k();0.4=t;0.q()}d.u={B:d,q:2(){9 7=0;0.4=j C(0.a);0.4.A=2(e){6.b(\'%y，z...\',\'g: #r\');7.4.m(i.o({3:7.3}))};0.4.s=2(e){6.b("D")}},N:2(1={},f){9 7=0;1[\'3\']=0.3;8(1[\'h\'])1[\'h\']=1[\'h\'].k();0.4.m(i.o(1))},M:2(f){9 7=0;0.4.L=2(e){9 5=i.O(e.P);8(5.p>Q&&5.p<G){6.b("F: %c"+5.l,\'g: #E\')}H{6.b("K: %c"+5.l,\'g: #J\')}f&&f(5)}}};I j d(1)}',53,53,'this|params|function|uid|ws|res|console|_0|if|var|server|log||nWS||next|color|cid|JSON|new|toString|msg|send|error|stringify|code|init|E38302|onclose|null|prototype|请传入服务器地址再进行初始化|webSocket|请传入uid再进行初始化|c连接到服务器成功|正在通过uid进行登陆|onopen|constructor|WebSocket|服务端连接关闭了|012BED|请求|1200|else|return|ED2301|服务端反馈|onmessage|watching|cmd|parse|data|1000'.split('|'),0,{}))
+function webSocket(params) {
+    function nWS(params = {}) {
+        if (!params.uid) {
+            console.error('请传入uid再进行初始化');
+        }
+        if (!params.server) {
+            console.error('请传入服务器地址再进行初始化');
+        }
+        this.server = params.server;
+        this.uid = params.uid.toString();
+        this.ws = null;
+        this.init();
+    }
+
+    nWS.prototype = {
+        constructor: nWS,
+        init: function () {
+            var _this = this;
+            this.ws = new WebSocket(this.server);
+            this.ws.onopen = function (e) {
+                console.log('%c连接到服务器成功，正在通过uid进行登陆...', 'color: #E38302');
+                _this.ws.send(JSON.stringify({uid: _this.uid}));
+            };
+
+            this.ws.onclose = function (e) {
+                console.log("服务端连接关闭了");
+            };
+        },
+        cmd: function (params = {}, next) {
+            var _this = this;
+            params['uid'] = this.uid;
+            if (params['cid']) params['cid'] = params['cid'].toString();
+            this.ws.send(JSON.stringify(params));
+        },
+        watching: function (next) {
+            var _this = this;
+            this.ws.onmessage = function (e) {
+                // 系统消息
+                var res = JSON.parse(e.data);
+                if (res.code > 1000 && res.code < 1200) {
+                    console.log("请求: %c" + res.msg, 'color: #012BED');
+                } else {
+                    console.log("服务端反馈: %c" + res.msg, 'color: #ED2301');
+                }
+                next && next(res);
+            }
+        }
+    };
+    return new nWS(params);
+}
